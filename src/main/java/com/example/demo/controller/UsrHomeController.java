a009d4408a6e02b3aac22f9ee1372005a492aa69
woooyoung <axdsw121@gmail.com>
2025-05-07 11:44:03 +0900

    여러가지 리턴타입

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,50 +1,87 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Controller
 public class UsrHomeController {
 
-	private int count;
-
-	public UsrHomeController() {
-		count = 0;
-	}
-
-	@RequestMapping("/usr/home/setCountValue")
+	@RequestMapping("/usr/home/getArticle")
 	@ResponseBody
-	public String setCountValue(int value) {
-		this.count = value;
-		return "count 값 " + value + "(으)로 초기화";
+	public Article getArticle() {
+
+		Article article = new Article(1, "제목1", "내용1");
+
+		return article;
 	}
 
-	@RequestMapping("/usr/home/setCount")
+	@RequestMapping("/usr/home/getList")
 	@ResponseBody
-	public String setCount() {
-		count = 0;
-		return "count 값 0으로 초기화";
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수 나이");
+		list.add("영수 나이");
+
+		return list;
 	}
 
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/getMap")
 	@ResponseBody
-	public int getCount() {
-		return count++;
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수 나이", 11);
+		map.put("영수 나이", 12);
+
+		return map;
 	}
 
-	@RequestMapping("/usr/home/main2")
+	@RequestMapping("/usr/home/getDouble")
 	@ResponseBody
-	public String showMain2() {
-		return "잘가";
+	public double getDouble() {
+
+		return 3.14;
 	}
 
-	@RequestMapping("/usr/home/main3")
+	@RequestMapping("/usr/home/getBoolean")
 	@ResponseBody
-	public int showMain3() {
-		int a = 1;
-		int b = 2;
+	public boolean getBoolean() {
 
-		return a + b;
+		return true;
 	}
+
+	@RequestMapping("/usr/home/getString")
+	@ResponseBody
+	public String getString() {
+
+		return "abc";
+	}
+
+	@RequestMapping("/usr/home/getInt")
+	@ResponseBody
+	public int getInt() {
+
+		return 1;
+	}
+
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Article {
+
+
+	int id;
+	String title;
+	String body;
 }
