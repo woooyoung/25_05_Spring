3872b946373a75c20e83502a3a3ea51d8f7fb9c4
woooyoung <axdsw121@gmail.com>
2025-05-09 16:09:52 +0900

    게시글 리스트 구현, jstl 설정

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -122,10 +123,13 @@ public class UsrArticleController {
 		return ResultData.newData(doWriteRd, "새로 작성된 게시글", article);
 	}
 
-	@RequestMapping("/usr/article/getArticles")
-	@ResponseBody
-	public ResultData<List<Article>> getArticles() {
+	@RequestMapping("/usr/article/list")
+	public String showList(Model model) {
+
 		List<Article> articles = articleService.getArticles();
-		return ResultData.from("S-1", "Article List", "게시글 리스트", articles);
+
+		model.addAttribute("articles", articles);
+
+		return "usr/article/list";
 	}
 }
