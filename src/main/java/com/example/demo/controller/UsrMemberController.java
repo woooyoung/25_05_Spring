07c1a2c4a51c332f0131b18de96100329a7a6429
woooyoung <axdsw121@gmail.com>
2025-05-12 16:49:22 +0900

    로그인 폼 구현 및 처리, 로그인 여부에 따른 header 버튼

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -37,9 +37,14 @@ public class UsrMemberController {
 		return ResultData.from("S-1", Ut.f("로그아웃 되었습니다"));
 	}
 
+	@RequestMapping("/usr/member/login")
+	public String showLogin() {
+		return "/usr/member/login";
+	}
+
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public ResultData<Member> doLogin(HttpSession session, String loginId, String loginPw) {
+	public String doLogin(HttpSession session, String loginId, String loginPw) {
 
 		boolean isLogined = false;
 
@@ -48,29 +53,29 @@ public class UsrMemberController {
 		}
 
 		if (isLogined) {
-			return ResultData.from("F-A", "이미 로그인 함");
+			return Ut.jsHistoryBack("F-A", "이미 로그인 함");
 		}
 
 		if (Ut.isEmptyOrNull(loginId)) {
-			return ResultData.from("F-1", "아이디를 입력해");
+			return Ut.jsHistoryBack("F-1", "아이디를 입력해");
 		}
 		if (Ut.isEmptyOrNull(loginPw)) {
-			return ResultData.from("F-2", "비밀번호를 입력해");
+			return Ut.jsHistoryBack("F-2", "비밀번호를 입력해");
 		}
 
 		Member member = memberService.getMemberByLoginId(loginId);
 
 		if (member == null) {
-			return ResultData.from("F-3", Ut.f("%s는(은) 없는 아이디야", loginId));
+			return Ut.jsHistoryBack("F-3", Ut.f("%s는(은) 없는 아이디야", loginId));
 		}
 
 		if (member.getLoginPw().equals(loginPw) == false) {
-			return ResultData.from("F-4", "비밀번호가 일치하지 않습니다");
+			return Ut.jsHistoryBack("F-4", "비밀번호가 일치하지 않습니다");
 		}
 
 		session.setAttribute("loginedMemberId", member.getId());
 
-		return ResultData.from("S-1", Ut.f("%s님 환영합니다", member.getNickname()), "로그인 한 회원", member);
+		return Ut.jsReplace("S-1", Ut.f("%s님 환영합니다", member.getNickname()), "/");
 	}
 
 	@RequestMapping("/usr/member/doJoin")
